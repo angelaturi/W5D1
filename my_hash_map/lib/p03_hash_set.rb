b9beae68dfa795d888aa26cedeef21385cb354ff
aturi <angelaturi99@gmail.com>
2021-03-22 15:04:53 -0400

    Add hash_set

--- my_hash_map/lib/p03_hash_set.rb
+++ my_hash_map/lib/p03_hash_set.rb
@@ -7,19 +7,26 @@ class HashSet
   end
 
   def insert(key)
+    self[key.hash] << key
+    @count += 1
+    resize! if num_buckets < count
   end
 
   def include?(key)
-    
+    self[key.hash].include?(key)
   end
 
   def remove(key)
+    if include?(key)
+      self[key.hash].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -27,5 +34,10 @@ class HashSet
   end
 
   def resize!
+    new_arr = Array.new(num_buckets * 2) { Array.new }
+    @store.each do |arr|
+      arr.each {|ele| new_arr[(ele.hash) % new_arr.length] << ele }
+    end
+    @store = new_arr
   end
 end
