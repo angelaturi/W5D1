e516285b64125aefc9411c74b665f50167f5226c
fdiengott <freddiengott@gmail.com>
2021-03-22 16:31:40 -0400

    add LinkedList#first, #last,#empty,#get,#append,#update,#each, and started #remove and Node#remove

--- my_hash_map/lib/p04_linked_list.rb
+++ my_hash_map/lib/p04_linked_list.rb
@@ -13,17 +13,37 @@ class Node
     "#{@key}: #{@val}"
   end
 
+  def inspect
+    "<LinkedList: #{self.to_s}, next:#{@next}, prev:#{@prev}>"
+  end
+
   def remove
+    previous = self.prev
+    if previous
+      previous.next = @next
+    end 
     
-    self.prev.next = (self.prev ? self.next : nil)
-    self.next.prev = (self.next ? self.prev : nil)
+    nex = self.next
+    if nex
+      nex.prev = @prev
+    end 
     
+    self.next = nil
+    self.prev = nil
+    
+    # self.prev.next = (self.prev ? self.next : nil)
+    # self.next.prev = (self.next ? self.prev : nil)
+
+    # self.next = nil
+    # self.prev = nil
+
+    self
   end
 end
 
 class LinkedList
-
-  # include Enumerable
+  attr_reader :head
+  include Enumerable
 
   def initialize
     @head = Node.new
@@ -38,44 +58,67 @@ class LinkedList
   end
 
   def first
+    empty? ? nil : head.next
   end
 
   def last
+    empty? ? nil : tail.prev
   end
 
   def empty?
+    head.next == tail    
   end
 
   def get(key)
+    each { |node| return node.val if node.key == key }
+    nil
   end
 
   def include?(key)
   end
 
   def append(key, val)
+    node = Node.new(key, val)
+
+    tail.prev.next = node
+    tail.prev = node
+
+    node.prev = tail.prev
+    node.next = tail
+    node
   end
 
   def update(key, val)
+    each do |node|
+      node.val = val if node.key == key
+      return node
+    end
   end
 
   def remove(key)
+    each do |node| 
+      if node.key == key
+        node.remove 
+        return node.val
+      end 
+    end 
+    nil
   end
 
   def each
-    node = head
-    loop do
+    node = head.next
+    while node != tail do
       yield node
       node = node.next
-      break if node == tail
     end
   end
 
   private
 
-  attr_reader :head, :tail
+  attr_reader :tail #:head, 
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
